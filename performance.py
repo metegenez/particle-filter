7e49fbb1aab7e7b93d8474e411ad66b78da4a74f
genez <metegenez@gmail.com>
2019-12-14 18:25:20 +0300

    resample yöntemlerinin performansa eklenmesi

--- performance.py
+++ performance.py
@@ -4,41 +4,41 @@ from itertools import product
 from matplotlib import pyplot as plt
 
 measurement_sigmas = list(np.sqrt(np.linspace(0.02, 5, 10)))
-Ns = [1000,5000, 10000, 15000, 20000]
+Ns = [50, 100,500,1000, 5000, 10000]
+resample_funcs = ["stratified_resample", "residual_resample", "systematic_resample"]
 process_noise = [np.sqrt(0.75)]
-trials = list(product(measurement_sigmas, process_noise, Ns, list(range(300))))
-performance_to_sigma = pickle.load(open(f'results.pickle', "rb"))
+trials = list(product(measurement_sigmas, process_noise, Ns, list(range(1000))))
+performance_to_sigma = pickle.load(open(f'results_low_particle.pickle', "rb"))
 
-particle = {}
 rmse_dict = {}
 ind_mse_dict = {}
+for func in resample_funcs:
+    plt.figure()
+    for particle_s in Ns:
+        rmse_dict = {}
+        ind_mse_dict = {}
+        for sigma in measurement_sigmas:
+            rmse_dict["{0:.2f}".format(sigma)] = []
+            ind_mse_dict["{0:.2f}".format(sigma)] = []
 
-for particle_s in Ns:
-    particle[particle_s] = []
-    for sigma in measurement_sigmas:
-        rmse_dict["{0:.2f}".format(sigma)] = []
-        ind_mse_dict["{0:.2f}".format(sigma)] = []
-    particle[particle_s].append(rmse_dict)
-    particle[particle_s].append(ind_mse_dict)
 
+        for i,each_try in enumerate(performance_to_sigma):
+            if each_try != [] and particle_s == trials[i][2]:
+                if func == trials[i][3].__name__:
+                # temp_particle = particle[trials[i][2]]
+                    rmse_dict["{0:.2f}".format(trials[i][0])].append(performance_to_sigma[i][0][0])
+                    ind_mse_dict["{0:.2f}".format(trials[i][0])].append(performance_to_sigma[i][0][1])
+            # particle[trials[i][2]] = temp_particle
 
-for i,each_try in enumerate(performance_to_sigma):
-    if performance_to_sigma[i] != []:
-        temp_particle = particle[trials[i][2]]
-        temp_particle[0]["{0:.2f}".format(trials[i][0])].append(performance_to_sigma[i][0][0])
-        temp_particle[1]["{0:.2f}".format(trials[i][0])].append(performance_to_sigma[i][0][1])
-
-del performance_to_sigma
-for particle_s in Ns:
-    y = []
-    x = []
-    for sigma in particle[particle_s][0].keys():
-        y.append(np.nanmean(np.array(particle[particle_s][0][sigma])))
-        x.append(sigma)
-    x.reverse()
-    y.reverse()
-    plt.plot(x,y, label=f"{particle_s} particle")
-    plt.title("RMSE Performance by Measurement Error")
-    plt.xlabel("Measurement Error")
-    plt.ylabel("RMSE")
-plt.show()
+        y = []
+        x = []
+        for sigma in rmse_dict.keys():
+            y.append(np.nanmean(np.array(rmse_dict[sigma])))
+            x.append(sigma)
+        x.reverse()
+        y.reverse()
+        plt.plot(x,y, label=f"{particle_s} particle")
+        plt.title(f"RMSE Performance by Measurement Error for {func}")
+        plt.xlabel("Measurement Error")
+        plt.ylabel("RMSE")
+    plt.show()
